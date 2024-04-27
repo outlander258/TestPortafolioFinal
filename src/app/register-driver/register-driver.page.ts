d399fd8066e88848434313988d7e213b8aad8c0d
outlander258 <jo.castillog@duocuc.cl>
2024-04-26 22:59:42 -0400

    validacionesForm

--- src/app/register-driver/register-driver.page.ts
+++ src/app/register-driver/register-driver.page.ts
@@ -26,16 +26,67 @@ export class RegisterDriverPage implements OnInit {
   }
 
 
-  ValidarFormDriver(){
-    console.log('Nombre : ' ,this.NameDriver)
-    console.log('Apellido : ' ,this.AppDriver)
-    console.log('RUN : ' , this.RunDriver)
-    console.log('Contraseña : ', this.PassDriver)
-    console.log('Confir Contaseña : ', this.ConfirmDriver)
+  ValidarFormDriver() {
+    console.log('Nombre : ', this.NameDriver);
+    console.log('Apellido : ', this.AppDriver);
+    console.log('RUN : ', this.RunDriver);
+    console.log('Contraseña : ', this.PassDriver);
+    console.log('Confir Contaseña : ', this.ConfirmDriver);
+  
+    if (!this.NameDriver) {
+      alert('Por favor, ingresa tu nombre.');
+      return;
+    }
+  
+    if (this.NameDriver.length < 8) {
+      alert('El nombre debe tener al menos 8 caracteres.');
+      return;
+    }
+  
+    if (!this.AppDriver) {
+      alert('Por favor, ingresa tu apellido');
+      return;
+    }
+  
+    if (this.AppDriver.length < 6) {
+      alert('El apellido debe contener al menos 6 caracteres');
+      return;
+    }
+  
+    if (!this.RunDriver) {
+      alert('Por favor, ingresa tu RUN ');
+      return;
+    }
+  
+    if (this.RunDriver.length < 7) {
+      alert('Ingresa tu RUN sin dígito verificador : EJEMPLO : 18033767');
+      return;
+    }
+  
+    if (!this.PassDriver) {
+      alert('Por favor, ingresa tu contraseña.');
+      return;
+    }
+  
+    if (this.PassDriver.length < 7) {
+      alert('La contraseña debe tener al menos 7 caracteres.');
+      return;
+    }
+  
+    
+    if (!/[A-Z]/.test(this.PassDriver)) {
+      alert('La contraseña debe contener al menos una mayúscula.');
+      return;
+    }
   
   
-    if (!this.PassDriver || !this.ConfirmDriver) {
-      alert('Por favor, ingresa y confirma tu contraseña.');
+    if (!/[$&+,:;=?@#|'<>.^*()%!-]/.test(this.PassDriver)) {
+      alert('La contraseña debe contener al menos un caracter especial.');
+      return;
+    }
+  
+    if (!this.ConfirmDriver) {
+      alert('Por favor, confirma tu contraseña.');
       return;
     }
   
@@ -44,10 +95,7 @@ export class RegisterDriverPage implements OnInit {
       return;
     }
   
-    console.log('contraseñas coinciden')
-  
-  
+    console.log('Contraseñas coinciden');
   }
-  
 
 }
--- src/app/register-user/register-user.page.ts
+++ src/app/register-user/register-user.page.ts
@@ -25,35 +25,79 @@ export class RegisterUserPage implements OnInit {
    
   }
 
-ValidarFormUser(){
-  console.log('Nombre : ' ,this.NameUser)
-  console.log('Apellido : ' ,this.AppUser)
-  console.log('RUN : ' , this.RunUser)
-  console.log('Contraseña : ', this.PassUser)
-  console.log('Confir Contaseña : ', this.ConfirmUser)
+  ValidarFormUser() {
+    console.log('Nombre : ', this.NameUser);
+    console.log('Apellido : ', this.AppUser);
+    console.log('RUN : ', this.RunUser);
+    console.log('Contraseña : ', this.PassUser);
+    console.log('Confir Contaseña : ', this.ConfirmUser);
+  
+    if (!this.NameUser) {
+      alert('Por favor, ingresa tu nombre.');
+      return;
+    }
+  
+    if (this.NameUser.length < 8) {
+      alert('El nombre debe tener al menos 8 caracteres.');
+      return;
+    }
+  
+    if (!this.AppUser) {
+      alert('Por favor, ingresa tu apellido');
+      return;
+    }
+  
+    if (this.AppUser.length < 6) {
+      alert('El apellido debe contener al menos 6 caracteres');
+      return;
+    }
+  
+    if (!this.RunUser) {
+      alert('Por favor, ingresa tu RUN ');
+      return;
+    }
+  
+    if (this.RunUser.length < 7) {
+      alert('Ingresa tu RUN sin dígito verificador : EJEMPLO : 18033767');
+      return;
+    }
+  
+    if (!this.PassUser) {
+      alert('Por favor, ingresa tu contraseña.');
+      return;
+    }
+  
+    const passUserString: string = this.PassUser.toString();
+  
+    if (passUserString.length < 7) {
+      alert('La contraseña debe tener al menos 7 caracteres.');
+      return;
+    }
+  
 
-
-  if (!this.PassUser || !this.ConfirmUser) {
-    alert('Por favor, ingresa y confirma tu contraseña.');
-    return;
+    if (!/[A-Z]/.test(passUserString)) {
+      alert('La contraseña debe contener al menos una mayúscula.');
+      return;
+    }
+  
+    if (!/[$&+,:;=?@#|'<>.^*()%!-]/.test(passUserString)) {
+      alert('La contraseña debe contener al menos un caracter especial.');
+      return;
+    }
+  
+    if (!this.ConfirmUser) {
+      alert('Por favor, confirma tu contraseña.');
+      return;
+    }
+  
+    if (passUserString !== this.ConfirmUser) {
+      alert('Las contraseñas no coinciden. Por favor, inténtalo de nuevo.');
+      return;
+    }
+  
+    console.log('Contraseñas coinciden');
   }
-
-  if (this.PassUser !== this.ConfirmUser) {
-    alert('Las contraseñas no coinciden. Por favor, inténtalo de nuevo.');
-    return;
-  }
-
-  console.log('contraseñas coinciden')
-
-
 }
-
-
-
-
-}
-
-
 
 
 
